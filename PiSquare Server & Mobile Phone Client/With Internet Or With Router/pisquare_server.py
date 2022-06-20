80d1a8e94342a456079f7737b5263e8e5de2bf14
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2022-06-20 17:04:35 +0530

    Add files via upload

---
+++ PiSquare Server & Mobile Phone Client/With Internet Or With Router/pisquare_server.py
@@ -0,0 +1,45 @@
+# import the socket library
+import socket            
+BUFFER_SIZE = 1024
+# next create a socket object
+def extract_ip():
+    st = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:       
+        st.connect(('10.255.255.255', 1))
+        IP = st.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        st.close()
+    return IP
+ip = extract_ip()
+
+data = "Server ip is : "+ip
+
+socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
+print ("Socket successfully created")
+socket_obj.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Choose any port number,make sure same port number is also in PiSquare
+port = 12420          
+ 
+# Next bind to the port
+socket_obj.bind(('', port))        
+print ("socket binded to %s" %(port))
+
+socket_obj.listen(5) # put the socket into listening mode  
+print ("socket is listening")
+
+conn, addr = socket_obj.accept() # Establish connection with client.
+print ('Server got connection from', addr )
+
+conn.send(data.encode()) # data send to client
+
+print (conn.recv(1024).decode()) # data receive from client
+
+while True:
+  n = input("enter data you need to send = ")
+  conn.send(n.encode())
+
+conn.close
+   
