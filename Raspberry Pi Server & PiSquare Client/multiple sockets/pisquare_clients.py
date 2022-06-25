5c409bb9c82adc496b85dee5b05414e3af52cd68
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2022-06-25 16:55:15 +0530

    Update pisquare_clients.py

--- Raspberry Pi Server & PiSquare Client/multiple sockets/pisquare_clients.py
+++ Raspberry Pi Server & PiSquare Client/multiple sockets/pisquare_clients.py
@@ -2,9 +2,9 @@ from machine import UART, Pin,SPI,I2C
 import utime,time
 from ssd1306 import SSD1306_I2C
 
-WiFi_SSID='Tech SB_2G'  # Wifi_SSID
-WiFi_password = 'jc643111h@'      # WiFi Password
-TCP_ServerIP = '192.168.29.73'  # IP of pisquare on which TCP server is running
+WiFi_SSID='Tech SB_2G'          # Wifi_SSID
+WiFi_password = 'jc643111h@'    # WiFi Password
+TCP_ServerIP = '192.168.29.73'  # Raspberry pi IP Address
 Port = '12420'                  # TCP Server Port (you can change)
 
 uart = UART(1,115200)           # Default Baud rate
