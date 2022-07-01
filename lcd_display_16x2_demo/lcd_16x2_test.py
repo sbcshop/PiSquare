07013af722ddb198926be1d874da7e5d824e07a5
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2022-07-01 17:49:23 +0530

    Add files via upload

---
+++ lcd_display_16x2_demo/lcd_16x2_test.py
@@ -0,0 +1,14 @@
+from machine import I2C, Pin
+from time import sleep
+from pico_i2c_lcd import I2cLcd
+i2c = I2C(0, sda=Pin(20), scl=Pin(21), freq=400000)
+
+I2C_ADDR = i2c.scan()[0]
+lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
+while True:
+    lcd.move_to(2,0)
+    lcd.putstr("SB COMPONENTS")
+    lcd.move_to(4,1)
+    lcd.putstr("PISQUARE")
+    
+  
