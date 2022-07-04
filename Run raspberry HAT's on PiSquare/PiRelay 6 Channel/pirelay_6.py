2ca3b41ece89bb48def7d3e6eae9256611d97f0b
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2022-07-04 14:56:43 +0530

    Update pirelay_6.py

--- Run raspberry HAT's on PiSquare/PiRelay 6 Channel/pirelay_6.py
+++ Run raspberry HAT's on PiSquare/PiRelay 6 Channel/pirelay_6.py
@@ -8,30 +8,30 @@ relay4 = machine.Pin(13, machine.Pin.OUT)
 relay5 = machine.Pin(14, machine.Pin.OUT)
 relay6 = machine.Pin(28, machine.Pin.OUT)
 
-relay1.value(0)#initally relay1 at LOW
-relay2.value(0)#initally relay2 at LOW
-relay3.value(0)#initally relay3 at LOW
-relay4.value(0)#initally relay4 at LOW
-relay5.value(0)#initally relay5 at LOW
-relay6.value(0)#initally relay6 at LOW
+relay1.value(0)#relay1 at LOW
+relay2.value(0)#relay2 at LOW
+relay3.value(0)#relay3 at LOW
+relay4.value(0)#relay4 at LOW
+relay5.value(0)#relay5 at LOW
+relay6.value(0)#relay6 at LOW
 
 time.sleep(1)
 
 while 1:
-    relay1.value(1)#initally relay1 at HIGH
-    relay2.value(1)#initally relay2 at HIGH
-    relay3.value(1)#initally relay3 at HIGH
-    relay4.value(1)#initally relay4 at HIGH
-    relay5.value(1)#initally relay5 at HIGH
-    relay6.value(1)#initally relay6 at HIGH
+    relay1.value(1)#relay1 at HIGH
+    relay2.value(1)#relay2 at HIGH
+    relay3.value(1)#relay3 at HIGH
+    relay4.value(1)#relay4 at HIGH
+    relay5.value(1)#relay5 at HIGH
+    relay6.value(1)#relay6 at HIGH
     
     time.sleep(1)
     
-    relay1.value(0)#initally relay1 at LOW
-    relay2.value(0)#initally relay2 at LOW
-    relay3.value(0)#initally relay3 at LOW
-    relay4.value(0)#initally relay4 at LOW
-    relay5.value(0)#initally relay5 at LOW
-    relay6.value(0)#initally relay6 at LOW
+    relay1.value(0)#relay1 at LOW
+    relay2.value(0)#relay2 at LOW
+    relay3.value(0)#relay3 at LOW
+    relay4.value(0)#relay4 at LOW
+    relay5.value(0)#relay5 at LOW
+    relay6.value(0)#relay6 at LOW
     
     time.sleep(1)
