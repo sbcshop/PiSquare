9572de835c45f99daecefe01e83407c2b33af1b2
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2022-07-04 12:43:31 +0530

    Add files via upload

---
+++ Run raspberry HAT's on PiSquare/Lora HAT/lora_receiver.py
@@ -0,0 +1,11 @@
+#Receiver code
+
+from machine import UART, Pin,SPI,I2C
+import time
+lora = UART(0,baudrate = 9600,tx = Pin(0),rx = Pin(1))
+
+while 1:
+        data_Read = lora.readline()#read data comming from other pico lora expansion
+        if data_Read is not None:
+                 print(data_Read)
+        time.sleep(0.2)
