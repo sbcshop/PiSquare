23585090be83c5a773e8dc8f17d7b2c2af28548f
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2022-07-04 12:53:01 +0530

    Update lora_receiver.py

--- Run raspberry HAT's on PiSquare/Lora HAT/lora_receiver.py
+++ Run raspberry HAT's on PiSquare/Lora HAT/lora_receiver.py
@@ -5,7 +5,7 @@ import time
 lora = UART(0,baudrate = 9600,tx = Pin(0),rx = Pin(1))
 
 while 1:
-        data_Read = lora.readline()#read data comming from other pico lora expansion
+        data_Read = lora.readline()#read data comming from other lora
         if data_Read is not None:
                  print(data_Read)
         time.sleep(0.2)
